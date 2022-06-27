399f751abcf7e760c77cab5c3dc54328c69eab37
Frodogorn <65722715+Frodogorn@users.noreply.github.com>
2022-06-27 15:03:03 +0200

    Add files via upload

--- javascript apy agregator/main.js
+++ javascript apy agregator/main.js
@@ -1,14 +1,43 @@
+function trierTableau(e, table){
+    let colonne = e.target.id;
+    console.log(e.target.id);
+    table = table.sort(function(a,b){
+        if(a[colonne]>b[colonne]){
+            return 1
+        }else if(a[colonne]==b[colonne]){
+            return 0
+        }else{
+            return -1
+        }
+    });
+    document.querySelector("tbody").innerHTML=""; //Supprime le tableau existant pour le remplacer ensuite par celui trié.
+    
+    genererTableau(table);
+}
+
+
+function ajouterBouttonEvent(table){
+    let i = 0;
+    for(let button of document.querySelectorAll("button")){
+        button.addEventListener("click", function(e){trierTableau(e, table)});
+        button.setAttribute("id", i);
+        i+=1;
+        console.log(button)
+    }
+}
+
 async function obtenirApyBeefy() {
     let response = await fetch("https://api.beefy.finance/apy");
     let apy_non_traite =  await response.json();
     let apys = [];
 
     for (let apy in apy_non_traite){
-        apys.push([apy.split("-"), [apy_non_traite[apy]]]);
+        apys.push([apy.split("-"), [apy_non_traite[apy]]]); // sépare les données du fichier json en trois colonnes: le nom de la plateforme, le pool de liquidité et le rendement.
     }
     apys = apys.map( x=>{return x[0].concat(x[1])});
     console.log(apys);
     apys = apys.map(x=>{
+        // rajoute un tiret entre les noms de coins des pools de liquidité, pour plus de lisibilité
         if (x.length == 4){
             x[1] += '-'+x[2];
             x.splice(2, 1)
@@ -18,7 +47,7 @@ async function obtenirApyBeefy() {
            }
            x.splice(2, x.length-1)
     }
-        x[x.length-1] = x[x.length-1]*100
+        x[x.length-1] *= 100
         return x
     })
     apys = apys.filter(x=> {return (x.length>2 && x[x.length-1]>0)}); //supprime les pools avec un rendement nul
@@ -35,7 +64,7 @@ async function obtenirTVLBeefy(tab){
     listeTVL = Object.values(listeTVL);
 
     for(let i=0; i<tab.length; i++){
-        vault = tab[i][0]+"-"+tab[i][1];
+        vault = tab[i][0]+"-"+tab[i][1]; // On récupère la clé de l'objet permettant d'obtenir la TVL d'un pool en réassemblant les données initiales
         for(let j=0; j<listeTVL.length; j++){
             if (listeTVL[j][vault] != undefined) {
                 tab[i].push(listeTVL[j][vault])
@@ -54,7 +83,7 @@ function estRendement(i, j, table){
 // vérifie si la cellule à l'indice j est à la colonne rendement, pour la convetir en pourcentage
     
     if (j==2){
-        return table[i][j] + "%"
+        return table[i][j].toFixed(2) + "%"
     } else {
         return table[i][j]
     }}
@@ -84,7 +113,7 @@ function genererTableau(table) {
        tbody.appendChild(ligne);
     
     }
-    
+    return table
 }
 
 
@@ -92,6 +121,7 @@ function genererTableau(table) {
 obtenirApyBeefy()
     .then(obtenirTVLBeefy)
     .then(genererTableau)
+    .then(ajouterBouttonEvent)
     
 
 
